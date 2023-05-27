5d572c3ee7795a0d0f6dc3a4a1386703d3257f4e
FlyingTonight <hikmasterr@gmail.com>
2023-05-27 17:36:11 +0500

    fixed

---
+++ send-email.php
@@ -0,0 +1,38 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 465;
+
+
+$mail->Username = "you@example.com";
+$mail->Password = "password";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("hikmasterr@gmail.com", "Hikmaster");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+// echo "Email sent";
+
+header("Location:sent.html");
